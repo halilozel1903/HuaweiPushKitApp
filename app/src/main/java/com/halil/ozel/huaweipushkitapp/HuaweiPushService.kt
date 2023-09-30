8725de9d0d0fc7dba415385127b809f62e820264
Halil Ozel <halilozel1903@gmail.com>
2023-09-30 18:57:11 +0300

    Gradle settings made.

--- app/src/main/java/com/halil/ozel/huaweipushkitapp/HuaweiPushService.kt
+++ app/src/main/java/com/halil/ozel/huaweipushkitapp/HuaweiPushService.kt
@@ -1,10 +1,13 @@
 package com.halil.ozel.huaweipushkitapp
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.PendingIntent
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.huawei.hms.push.HmsMessageService
@@ -45,6 +48,20 @@ class HuaweiPushService : HmsMessageService() {
             .setOnlyAlertOnce(true)
             .setColor(this.resources.getColor(R.color.black, null))
             .build()
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return
+        }
         notificationManager.notify(1, notification)
     }
 
