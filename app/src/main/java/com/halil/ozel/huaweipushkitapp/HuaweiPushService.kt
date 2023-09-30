32e8fd268b8ee88c11875ba418b7a01bf5320b62
Halil Ozel <halilozel1903@gmail.com>
2023-09-30 18:59:09 +0300

    CheckSelfPermission settings added.

--- app/src/main/java/com/halil/ozel/huaweipushkitapp/HuaweiPushService.kt
+++ app/src/main/java/com/halil/ozel/huaweipushkitapp/HuaweiPushService.kt
@@ -53,16 +53,8 @@ class HuaweiPushService : HmsMessageService() {
                 Manifest.permission.POST_NOTIFICATIONS
             ) != PackageManager.PERMISSION_GRANTED
         ) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return
+            notificationManager.notify(1, notification)
         }
-        notificationManager.notify(1, notification)
     }
 
     companion object {
